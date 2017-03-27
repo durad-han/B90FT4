ab2f43d1b2337ac4e6eb49ef2fd2964a826efc4f
chak2156 <chak2156@gmail.com>
2017-03-27 11:14:13 +0900

    workoutCtrl anno added

--- b90ft4/src/main/java/b90ft4/web/workout/controller/WorkoutController.java
+++ b90ft4/src/main/java/b90ft4/web/workout/controller/WorkoutController.java
@@ -1,5 +1,19 @@
 package b90ft4.web.workout.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import b90ft4.web.workout.service.WorkoutService;
+
+@RequestMapping("/workout")
+@Controller
 public class WorkoutController {
+	
+	private WorkoutService ws;
+	
+	@RequestMapping("/camInit.do")
+	public void camInit(){
+		System.out.println("camInit started");
+	}
 
 }
