fe7bd0d7467a3a09df694fd97e2a6d1add4193c0
GwangSon <tcmender@naver.com>
2017-03-30 15:04:24 +0900

    메인에서 다리어리 수정

--- b90ft4/src/main/webapp/web/js/main/main.js
+++ b90ft4/src/main/webapp/web/js/main/main.js
@@ -1,5 +1,5 @@
 $("#menu01").on("click", function(){location.href="/b90ft4/schedule/schedule.do"});
-$("#menu02").on("click", function(){location.href="/b90ft4/diary/diary.do"});
+$("#menu02").on("click", function(){location.href="/b90ft4/diary/list.do"});
 $("#menu03").on("click", function(){location.href="/b90ft4/accountbook/main.do"});
 $("#menu04").on("click", function(){location.href="/b90ft4/workout/workout.do"});
 $("#menu05").on("click", function(){location.href="/b90ft4/main/main.do"});
