403ed95b4e90fc05df72beaabf14913c0ffccf7d
chak2156 <chak2156@gmail.com>
2017-05-08 20:02:51 +0900

    workoutCal.html to .jsp

---
+++ b90ft4/src/main/webapp/WEB-INF/jsp/workout/workoutCal.jsp
@@ -0,0 +1,156 @@
+<%@ page contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Freelancer - Start Bootstrap Theme</title>
+    <!-- Bootstrap Core CSS -->
+<%--     <link href="${pageContext.request.contextPath}/web/bootstrap/freelancer/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> --%>
+    <link type="text/css" rel="stylesheet" href="${pageContext.request.contextPath}/web/css/common/bootstrap.min.css">
+	<c:import url ="/WEB-INF/jsp/common/frameInclude.jsp"/>
+    <!-- Theme CSS -->
+    <link href="${pageContext.request.contextPath}/web/bootstrap/freelancer/css/freelancer.min.css" rel="stylesheet">
+    <link href="${pageContext.request.contextPath}/web/bootstrap/freelancer/css/main.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+  	<script src="http://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <!-- Custom Fonts -->
+    <link href="${pageContext.request.contextPath}/web/bootstrap/freelancer/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+	
+</head>
+
+<body id="page-top" class="index">
+
+<%-- <div class="container" style='background:url("${pageContext.request.contextPath}/web/image/accountBook/가계부 배경2.jpg") no-repeat;background-size:100%'> --%>
+<div class="container">
+
+<div id="skipnav"><a href="#maincontent">Skip to main content</a></div>
+
+    <!-- Navigation -->
+   
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="main.do">자기 관리</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                        
+                    <!-- 서브메뉴 드랍다운 -->
+                    <li class = "dropdown">
+                    
+					      <a class = "dropdown-toggle" data-toggle = "dropdown" href = "#">
+					          가계부 <span class = "caret"></span>
+					      </a>
+					      
+					      <ul class = "dropdown-menu">
+					         <li><a href = "#">지출/수입</a></li>
+					         <li><a href = "#">대입금/차입금</a></li>
+					         <li><a href = "#">설정</a></li>
+					      </ul>
+					      
+				   </li>
+                        
+                    <li class="page-scroll">
+                        <a href="#about">다이어리</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#contact">스케쥴러</a>
+                    </li>
+                </ul>
+                
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Portfolio Grid Section -->
+    <section id="portfolio">
+        <div class="container">
+                	
+     <div>
+<h2>오늘 섭취 칼로리 입력</h2><button>음식별 섭취 칼로리 알아보기</button><button onclick='callIntakeCal()'>오늘 운동 전에 먹은 음식에서 받아오기</button><br>
+<input id='setConsumeCal' type='text'>kcal<br>
+<h2>오늘 소비 칼로리 입력(기초대사량 제외)</h2><button>기초대사량 구하기</button><button onclick='callSpentCal()'>오늘 한 운동에서 받아오기</button><br>
+<input id='setSpendCal'type='text'>kcal<br>
+
+<button onclick='setCal()'>입력</button><button>저장</button>
+</div>
+<!-- <div id='BMContainer' hidden="true"> -->
+<div id='BMContainer'>
+키:<input id='setUserHeight' type='text'>cm<br>
+몸무게:<input id='setUserWeight' type='text'>kg<br>
+나이:<input id='setUserAge' type='text'>세<br>
+성별:<input type="radio" name="gender" id="genderMale" value="male" checked> 남 
+	<input type="radio" name="gender" id="genderFemale" value="female"> 여<br>
+기초대사량:<input id ='BMView' type='text'><br>
+<button onclick = 'calculateBM()'>계산</button>
+<button onclick = 'setBM()'>오늘 소비 칼로리에 적용</button>
+<button onclick = 'callBM()'>내 정보 불러오기</button>
+<button onclick = 'saveBM()'>내 정보에 저장</button>
+<button onclick = 'deleteBM()'>내 정보 삭제</button>
+</div>
+<div class='caption'>칼로리 계산</div>
+<div id='bar-chart-stack' style='width: 50%; height:300px'></div>
+<script src="${pageContext.request.contextPath}/web/js/workout/workoutCal.js"></script>
+		        	
+        </div>
+    </section>
+    
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+    
+<%-- 
+    <!-- jQuery -->
+    <script src="${pageContext.request.contextPath}/web/bootstrap/freelancer/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="${pageContext.request.contextPath}/web/bootstrap/freelancer/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="${pageContext.request.contextPath}/web/bootstrap/freelancer/js/jqBootstrapValidation.js"></script>
+    <script src="${pageContext.request.contextPath}/web/bootstrap/freelancer/js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="${pageContext.request.contextPath}/web/bootstrap/freelancer/js/freelancer.min.js"></script>
+	<script src="${pageContext.request.contextPath}/web/js/common/validUtil.js"></script>
+	
+    <!-- 차트 js -->
+	<script src="${pageContext.request.contextPath}/web/bootstrap/KAdmin-Dark/script/jquery.flot.js"></script>
+	<script src="${pageContext.request.contextPath}/web/bootstrap/KAdmin-Dark/script/jquery.flot.pie.js"></script>
+	<script src="${pageContext.request.contextPath}/web/bootstrap/KAdmin-Dark/script/jquery.flot.resize.js"></script>
+--%>
+</div>
+</body>
+
+</html>
--- b90ft4/src/main/webapp/web/js/workout/workoutCal.js
+++ b90ft4/src/main/webapp/web/js/workout/workoutCal.js
@@ -1,4 +1,9 @@
-//bar-chart-stack  
+	var totalSpentCal = 0;   
+	var totalIntakeCal = 0;
+	var today = $.datepicker.formatDate("yy-mm-dd",new Date());
+	var BM = 0;
+	var userId = 'tester01';
+//bar-chart-stack 
 var d4_1 = [["Jan", 126],["Feb", 73],["Mar", 94],["Apr", 54],["May", 92],["Jun", 141],["Jul", 29],["Aug", 44],["Sep", 30],["Oct", 40],["Nov", 67],["Dec", 92]];
     var d4_2 = [["Jan", 58],["Feb", 61],["Mar", 46],["Apr", 35],["May", 55],["Jun", 46],["Jul", 57],["Aug", 80],["Sep", 100],["Oct", 91],["Nov", 35],["Dec", 57]];
     $.plot("#bar-chart-stack", [{
@@ -39,3 +44,230 @@ var d4_1 = [["Jan", 126],["Feb", 73],["Mar", 94],["Apr", 54],["May", 92],["Jun",
         },
         shadowSize: 0
     });
+
+
+
+	function callIntakeCal(){
+	$.ajax({
+		url:"selectWorkoutStatisticsList.do",
+		dataType:"json",
+		data : {"today" : today }
+		}).done(function(result){
+			console.dir(result);
+			console.dir($('#setConsumeCal').val());
+			for(var i = 0 ; i < result.length ; i++){
+				totalIntakeCal += parseInt(result[i].intakeCal);
+			}
+			$('#setConsumeCal').val(totalIntakeCal);
+		});
+	}
+	
+function callSpentCal(){
+	totalSpentCal = 0;
+	console.log("callSpentCal loaded");
+	console.log("today : "+ today);
+	$.ajax({
+		url:"selectWorkoutStatisticsList.do",
+		dataType:"json",
+		data : {"today" : today }
+		}).done(function(result){
+			console.dir(result);
+			for(var i = 0 ; i < result.length ; i++){
+				totalSpentCal += parseInt(result[i].spentCal);
+			}
+			$('#setSpendCal').val(totalSpentCal);
+		});
+	}
+  
+   
+   var spendCal1 = 500; 
+   var consumeCal1 = 1000;
+   var leftCal1 = consumeCal1 - spendCal1;
+   var overCal1 = spendCal1 - consumeCal1;
+   if (spendCal1 < consumeCal1){
+	   overCal1 = 0;
+   }
+
+   var spendCal2 = 1000; 
+   var consumeCal2 = 1000;
+   var leftCal2 = consumeCal2 - spendCal2;
+   var overCal2 = spendCal2 - consumeCal2;
+   
+   if (spendCal2 < consumeCal2){
+	   overCal2 = 0;
+   }
+
+   var spendCal3 = 1500; 
+   var consumeCal3 = 1000;
+   var leftCal3 = consumeCal3 - spendCal3;
+   var overCal3 = spendCal3 - consumeCal3;
+   if (spendCal3 < consumeCal3){
+	   overCal3 = 0;
+   }
+   
+   
+   var spendCal4 = 0;
+   var consumeCal4 = 0;
+   var leftCal4 = 0;
+   var overCal4 = 0;
+   function setCal(){
+   spendCal4 = $("#setSpendCal").val();
+   console.log("consume:"+$("#setConsumeCal").val());
+   consumeCal4 = $("#setConsumeCal").val();
+   leftCal4 = consumeCal4 - spendCal4;
+   overCal4 = spendCal4 - consumeCal4;
+   if (spendCal4 < consumeCal4){
+	   overCal4 = 0;
+   }
+   barChartStack();
+   console.log("차트스택 빠져나온 spendcal4 : "+spendCal4);
+
+   }
+   
+ //bar-chart-stack  
+   var d4_1 = [["날짜1", spendCal1],["날짜2", spendCal2],["날짜3", spendCal3],[today, spendCal4]];
+   var d4_2 = [["날짜1", leftCal1],["날짜2", leftCal2],["날짜3", leftCal3],[today, leftCal4]];
+   var d4_3 = [["날짜1", overCal1],["날짜2", overCal2],["날짜3", overCal3],[today, overCal4]];
+   function barChartStack(){
+	   d4_1 = [["날짜1", spendCal1],["날짜2", spendCal2],["날짜3", spendCal3],[today, spendCal4]];
+	   d4_2 = [["날짜1", leftCal1],["날짜2", leftCal2],["날짜3", leftCal3],[today, leftCal4]];
+	   d4_3 = [["날짜1", overCal1],["날짜2", overCal2],["날짜3", overCal3],[today, overCal4]];
+	   
+	   console.log("차트스택 안에있는 spendcal4 : "+spendCal4);
+		console.dir(d4_1);
+       $.plot("#bar-chart-stack", [{
+           data: d4_1,
+           label: "소비 칼로리",
+           color: "#f15f5f"
+       },{
+           data: d4_2,
+           label: "섭취 칼로리",
+           color: "#3DB9D3"
+       },{
+           data: d4_3,
+           label: "초과 소비 칼로리",
+           color: "#ffce54"
+       }], {
+           series: {
+               stack: !0,
+               bars: {
+                   align: "center",
+                   lineWidth: 0,
+                   show: !0,
+                   barWidth: .6,
+                   fill: .9
+               }
+           },
+           grid: {
+               borderColor: "#fafafa",
+               borderWidth: 1,
+               hoverable: !0
+           },
+           tooltip: !0,
+           tooltipOpts: {
+               content: "%x : %y",
+               defaultTheme: false
+           },
+           xaxis: {
+               tickColor: "#fafafa",
+               mode: "categories"
+           },
+           yaxis: {
+               tickColor: "#fafafa"
+           },
+           shadowSize: 0
+       });
+		console.dir(d4_1);
+   }
+   barChartStack();
+   
+   //BM 관련 함수
+//<button onclick = 'calculateBM()'>계산</button>
+   function calculateBM(){
+/* 	   
+	   <div id='BMContainer' hidden="true">
+	   키:<input id='setUserHeight' type='text'>cm<br>
+	   몸무게:<input id='setUserWeight' type='text'>kg<br>
+	   나이:<input id='setUserAge' type='text'><br>
+	   기초대사량:<input id ='BMView'type='text'><br>
+	    */
+
+	    var userHeight = Number($("#setUserHeight").val());
+	    var userWeight = Number($("#setUserWeight").val());
+	    var userAge = Number($("#setUserAge").val());
+	    var userGender = $('input:radio[name=gender]:checked').val();
+	    BM = 66.47 + ( 13.175 * userWeight ) + ( 5 * userHeight ) - ( 6.76 *  userAge );
+	    
+	    if ( userGender == 'female'){
+	    	BM = 655.1 + (9.56 * userWeight ) + (1.85 * userHeight ) - ( 4.68 * userAge );
+	    	tempflag = 2;
+	    }
+	    $('#BMView').val(BM);
+	   
+	   
+   }
+//<button onclick = 'setBM()'>오늘 소비 칼로리에 적용</button>
+	function setBM(){
+		var updateSpendCalVal = Number($('#setSpendCal').val()) + Number(BM);
+		$('#setSpendCal').val(updateSpendCalVal);
+	}
+//<button onclick = 'callBM()'>불러오기</button>
+	function callBM(){
+		$.ajax({
+			url:"selectWorkoutUserInfo.do",
+			dataType:"json",
+			data:{"userId" : userId}
+
+			}).done(function(result){
+				//console.dir(result);
+				$('#setUserHeight').val(Number(result.userHeight));
+				$('#setUserWeight').val(Number(result.userWeight));
+				$('#setUserAge').val(Number(result.userAge));
+				if(result.userGender == 'male'){
+					$('#genderFemale').removeClass('checked');
+					$('#genderMale').addClass('checked');
+				}
+				else{
+					$('#genderMale').removeClass('checked');
+					$('#genderFemale').addClass('checked');
+				}
+				
+			});
+	}
+//<button onclick = 'saveBM()'>저장</button>
+	function saveBM(){
+		
+		var userHeight = Number($("#setUserHeight").val());
+	    var userWeight = Number($("#setUserWeight").val());
+	    var userAge = Number($("#setUserAge").val());
+	    var userGender = $('input:radio[name=gender]:checked').val();
+		
+		$.ajax({
+			url:"updateWorkoutUserInfo.do",
+			dataType:"json",
+			data : {
+					"userId" : userId,
+					"userHeight" : userHeight,
+					"userWeight" : userWeight,
+					"userAge" : userAge,
+					"userGender" : userGender
+					}
+			
+			}).done(function(){
+				console.log("update is done.");
+			});
+		
+	}
+//<button onclick = 'deleteBM()'>삭제</button>
+	function deleteBM(){
+		$.ajax({
+			url:"deleteWorkoutUserInfo.do",
+			dataType:"json",
+			data:{"userId" : userId}
+			
+			}).done(function(){
+				console.log("delete is done.");
+			});
+		
+	}
+   
